1ffeb8fccda262858a39b03e1d23f1948765a6e6
Suchi Siwach <46921160+Suchitech02@users.noreply.github.com>
2020-09-12 08:08:00 +0200

    BookController & ConfigureService in startup

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,7 +1,27 @@
+using System.Threading.Tasks;
+using TokenWebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace TokenWebAPI.Controllers
 {
-    public class BookController
+   [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController: ControllerBase
     {
-        
+        private readonly jwt1Context _context;
+        public BookController(jwt1Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var data = await _context.TblBook.ToListAsync();
+            return Ok(data);
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using TokenWebAPI.Models;
+using TokenWebAPI.Repositories;
 
 namespace TokenWebAPI
 {
@@ -25,7 +32,24 @@ namespace TokenWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+             
+             services.AddDbContext<jwt1Context>(options => options.UseSqlServer(Configuration.GetConnectionString("SQLConnection")));
+            services.AddCors();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+                
+             services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
